237f7377d91ffebb31789334ed9f281ab9bfda32
Gabriel Birnbaum <gabriel@barbra.io>
2020-06-09 09:49:22 +0200

    Updates

---
+++ precache-manifest.6684c8122d9910558b6bc0daefd21a2f.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f56f2e686cee60be7ef2e550d0b592ea",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "88eaedb33463e54e4ae9",
+    "url": "/portfolio/static/css/main.18ca2701.chunk.css"
+  },
+  {
+    "revision": "3e416789baba60e007de",
+    "url": "/portfolio/static/js/2.a617a02f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.a617a02f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88eaedb33463e54e4ae9",
+    "url": "/portfolio/static/js/main.8005429f.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "a1d65c5b1d1547a34e3e54f964d8f4cd",
+    "url": "/portfolio/static/media/Sentinel-Medium.a1d65c5b.otf"
+  },
+  {
+    "revision": "52c1236b9ffa704d35711d4277a85f1d",
+    "url": "/portfolio/static/media/applicantOverview.52c1236b.svg"
+  },
+  {
+    "revision": "c9c8a215a06cd65d7db7b03a5ede786e",
+    "url": "/portfolio/static/media/companyDisplay.c9c8a215.png"
+  },
+  {
+    "revision": "2409c81e19d509890a529f8579d55437",
+    "url": "/portfolio/static/media/createJobVideo.2409c81e.mp4"
+  },
+  {
+    "revision": "4093cd9d1af66b9db160fb0f00ff0681",
+    "url": "/portfolio/static/media/flashcardsVideo.4093cd9d.mp4"
+  },
+  {
+    "revision": "3b9781d602873248d54a65c5d222914b",
+    "url": "/portfolio/static/media/highlightsOverviewVideo.3b9781d6.mp4"
+  },
+  {
+    "revision": "21e22423dafed7a24c79aab1e8ce5f50",
+    "url": "/portfolio/static/media/highlightsVideo.21e22423.mp4"
+  },
+  {
+    "revision": "0022664760d35d747422cdfa0b098315",
+    "url": "/portfolio/static/media/mafiaBird.00226647.png"
+  },
+  {
+    "revision": "1a6cfdf376be39dbce8ca87a85e9c4be",
+    "url": "/portfolio/static/media/mafiaworm.1a6cfdf3.png"
+  },
+  {
+    "revision": "54d8051df27582c489a2f63d00afc79b",
+    "url": "/portfolio/static/media/newApplicants.54d8051d.png"
+  },
+  {
+    "revision": "f6863e86868b8b987830f04ebadd87fe",
+    "url": "/portfolio/static/media/offer.f6863e86.svg"
+  },
+  {
+    "revision": "b1e8c84778612e5a4eeda49f79af9570",
+    "url": "/portfolio/static/media/orbiteer1.b1e8c847.mp4"
+  },
+  {
+    "revision": "b8e2f416c8951fb0a7742e4462340f68",
+    "url": "/portfolio/static/media/orbiteer1.b8e2f416.svg"
+  },
+  {
+    "revision": "73c502eaf89253ac712e6e7f1493b8f8",
+    "url": "/portfolio/static/media/orbiteer2.73c502ea.mp4"
+  },
+  {
+    "revision": "e3599be9b5c2b8c0caa0be0d370335f5",
+    "url": "/portfolio/static/media/orbiteer2.e3599be9.svg"
+  },
+  {
+    "revision": "601c12a079822f7f70a961fc4ef97623",
+    "url": "/portfolio/static/media/search.601c12a0.svg"
+  },
+  {
+    "revision": "df9b5ea2b81495c6fff2b3d2b1e43a3c",
+    "url": "/portfolio/static/media/setAppointment.df9b5ea2.svg"
+  },
+  {
+    "revision": "5e6a8ffef804de7a6871be0c4ced9f9c",
+    "url": "/portfolio/static/media/sidebarVideo.5e6a8ffe.mp4"
+  },
+  {
+    "revision": "4ba8fb90a2c3ee6b58a8a0e72dc2021b",
+    "url": "/portfolio/static/media/slashVideo.4ba8fb90.mp4"
+  }
+]);
